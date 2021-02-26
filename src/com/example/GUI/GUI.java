b517a6a860f89c6835027504ca8d255fc7a072ac
Kyle Lieu <klieu1@luc.edu>
2021-02-26 13:41:20 -0600

    Working GUI using JFrame

--- src/com/example/GUI/GUI.java
+++ src/com/example/GUI/GUI.java
@@ -1,23 +1,83 @@
 package com.example.GUI;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 
+class GUI extends JFrame{
+    JPanel panel=new JPanel();
 
-public class GUI {
+    JLabel instruction=new JLabel("Please Select a Game Below");
+    //ADD CHOICES TO DROPDOWN
+    JLabel gameSelect=new JLabel("Games:");
+    String choices[] = {
+            "Snake",
+            "Hangman",
+            "Tic Tac Toe",
+            "",
+            "",
+            "",
+    };
+    //ADD BUTTONS
+    public JComboBox choicesButton = new JComboBox(choices);
+    public JButton beginButton=new JButton("Begin!");
+    public JButton exit = new JButton("Exit");
+    //INIT GUI
     public static void main(String[] args){
-        System.out.println("WELCOME");
-        JFrame frame = new JFrame("GAME CATALOG");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(625,300);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
-        frame.setVisible(true);
+        new GUI();
+    }
+    //CREATE GUI
+    public GUI(){
+        super("GitPic");
+        setSize(625,300);
+        setResizable(true);
+        panel.add(instruction);
+        panel.add(gameSelect);
+        panel.add(choicesButton);
+        panel.add(beginButton);
+        panel.add(exit);
+        add(panel);
+
+        setVisible(true);
+        //EXIT PROGRAM
+        exit.addActionListener(new ActionListener() {//add event listener to exit
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);//close
+            }
+        });
+        //DIRECTORY CHOICES
+        choicesButton.addActionListener(new ActionListener(){// add event listener to choicesButton
+            public void actionPerformed(ActionEvent e) {
+                //fetch user choice from drop down
+                JComboBox cb = (JComboBox) e.getSource();
+                //set choices equal to selected item in JComboBox cb... Updates live!
+                String choices = (String) cb.getSelectedItem();
+                // if choice is downloads, get downloads location and set as folderPath
+                if ((choices).equals("Snake")) {
+
+                }
+                // if choice is desktop, get desktop location and set as folderPath
+                if ((choices).equals("Hangman")) {
+
+                }
+                // if choice is documents, get documents location and set as folderPath
+                if ((choices).equals("Tic Tac Toe")) {
+
+                }
+                // if choice is pictures, get pictures location and set as folderPath
+                if ((choices).equals("")) {// if choice is pictures, get pictures location and set as folderPath
+
+                }
+                // if choice is root, get root location and set as folderPath
+                if ((choices).equals("")) {
+
+                }
+            }
+        });
     }
 }
