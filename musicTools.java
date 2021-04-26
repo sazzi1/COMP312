834f54251c87fb4af4adf75f5afc66aa598eb271
VivPautsch <viviana.pautsch@gmail.com>
2021-04-26 13:56:40 -0500

    updated snake game

---
+++ musicTools.java
@@ -0,0 +1,36 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import java.io.File;
+
+
+public class musicTools {
+	
+	void playMusic(String musicLocation) {
+		
+		try {
+			File musicPath = new File(musicLocation);
+			
+			if(musicPath.exists()) {
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInput);
+				clip.start();
+				clip.loop(Clip.LOOP_CONTINUOUSLY);		
+				
+			}
+			else {
+				System.out.println("Can't find file!!");
+			}
+			
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
